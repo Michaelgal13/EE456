fd0bf562101082f11e546c8515be908fd613af20
Michaelgal13 <41348138+Michaelgal13@users.noreply.github.com>
2020-10-21 18:47:51 -0700

    Update radio_rfm9x.py

--- radio_rfm9x.py
+++ radio_rfm9x.py
@@ -1,3 +1,6 @@
+# python3 radio_rfm9x.py
+
+
 """
 Example for using the RFM9x Radio with Raspberry Pi.
 
@@ -31,16 +34,16 @@ btnC.direction = Direction.INPUT
 btnC.pull = Pull.UP
 
 # Create the I2C interface.
-i2c = busio.I2C(board.SCL, board.SDA)
+# i2c = busio.I2C(board.SCL, board.SDA)
 
 # 128x32 OLED Display
-reset_pin = DigitalInOut(board.D4)
-display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, reset=reset_pin)
+# reset_pin = DigitalInOut(board.D4)
+# display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, reset=reset_pin)
 # Clear the display.
-display.fill(0)
-display.show()
-width = display.width
-height = display.height
+# display.fill(0)
+# display.show()
+# width = display.width
+# height = display.height
 
 # Configure LoRa Radio
 CS = DigitalInOut(board.CE1)
@@ -53,46 +56,46 @@ prev_packet = None
 while True:
     packet = None
     # draw a box to clear the image
-    display.fill(0)
-    display.text('RasPi LoRa', 35, 0, 1)
+    # display.fill(0)
+    # display.text('RasPi LoRa', 35, 0, 1)
 
     # check for packet rx
     packet = rfm9x.receive()
     if packet is None:
-        display.show()
-        display.text('- Waiting for PKT -', 15, 20, 1)
+        # display.show()
+        # display.text('- Waiting for PKT -', 15, 20, 1)
     else:
         # Display the packet text and rssi
-        display.fill(0)
+        # display.fill(0)
         prev_packet = packet
         packet_text = str(prev_packet, "utf-8")
-        display.text('RX: ', 0, 0, 1)
-        display.text(packet_text, 25, 0, 1)
+        # display.text('RX: ', 0, 0, 1)
+        # display.text(packet_text, 25, 0, 1)
         print(packet_text);
         time.sleep(1)
 
     if not btnA.value:
         # Send Button A
-        display.fill(0)
+        # display.fill(0)
         button_a_data = bytes("Button A!\r\n","utf-8")
         rfm9x.send(button_a_data)
-        display.text('Sent Button A!', 25, 15, 1)
+        # display.text('Sent Button A!', 25, 15, 1)
         print('Sent Button A!')
     elif not btnB.value:
         # Send Button B
-        display.fill(0)
+        # display.fill(0)
         button_b_data = bytes("Button B!\r\n","utf-8")
         rfm9x.send(button_b_data)
-        display.text('Sent Button B!', 25, 15, 1)
+        # display.text('Sent Button B!', 25, 15, 1)
         print('Sent Button B!')
     elif not btnC.value:
         # Send Button C
-        display.fill(0)
+        # display.fill(0)
         button_c_data = bytes("Button C!\r\n","utf-8")
         rfm9x.send(button_c_data)
-        display.text('Sent Button C!', 25, 15, 1)
+        # display.text('Sent Button C!', 25, 15, 1)
         print('Sent Button C!')
 
 
-    display.show()
+    # display.show()
     time.sleep(0.1)
